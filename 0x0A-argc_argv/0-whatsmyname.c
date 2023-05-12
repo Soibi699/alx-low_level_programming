90ab5252fba736f920857b876308167af9fe0573
Soibi699 <ephraimgeorgesoibi@gmsil.com>
2023-05-12 13:56:43 -0700

    My name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-printf("%s\n", *argv);
 
-return (0);
+int main(int argc __attribute__((unused)), char *argv[])
+
+{
+	printf("%s\n", *argv);
+
+	return (0);
 }
